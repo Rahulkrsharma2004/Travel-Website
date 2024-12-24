1ad12004badcf0ec4af3f24e5c481da3d344d56a
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-12-24 10:10:55 +0530

    model

--- backend-web/models/Trip.js
+++ backend-web/models/Trip.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const tripSchema = new mongoose.Schema({
+  // image: {
+  //   type: String,
+  //   required: true,
+  // },
   name: {
     type: String,
     required: true,
